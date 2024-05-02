e57fb11d1f46410cc0fe3de8d23e91b0c969e04c
brandonviorato <brandonviorato@outlook.com>
2024-05-02 13:36:23 -0400

    Completed survey route and created data-layer

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Require
+// Require necessary files
 require_once ('vendor/autoload.php');
+require_once('model/data-layer.php');
 
 // Instantiate the F3 Base class
 $f3 = Base::instance();
@@ -23,8 +24,14 @@ $f3->route('GET /', function() {
 });
 
 // survey route
-$f3->route('GET /survey', function() {
+$f3->route('GET /survey', function($f3) {
     // echo '<h1>Hello survey!</h1>';
+
+
+    // Get the data from the model
+    // and add it to the F3 hive
+    $options = getOptions();
+    $f3->set('options', $options);
 
     // Render a view page
     $view = new Template();
---
+++ model/data-layer.php
@@ -0,0 +1,10 @@
+<?php
+
+/* This is my data layer.
+ * It belongs to the Model
+ */
+
+// Get the checkbox options
+function getOptions() {
+    return array('This midterm is easy', 'I like midterms', 'Today is monday');
+}
