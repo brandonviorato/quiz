2ba8f5028c53e2ef64f303c76f28e80393e29eac
brandonviorato <brandonviorato@outlook.com>
2024-05-02 13:49:44 -0400

    completed summary route

--- index.php
+++ index.php
@@ -24,9 +24,28 @@ $f3->route('GET /', function() {
 });
 
 // survey route
-$f3->route('GET /survey', function($f3) {
+$f3->route('GET|POST /survey', function($f3) {
     // echo '<h1>Hello survey!</h1>';
 
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        // Get the data from the post array
+        if (isset($_POST['name']) and isset($_POST['options'])) {
+            $name = $_POST['name'];
+            $selectedOptions = implode(", ", $_POST['options']);
+        }
+        else {
+            $name = "Not entered";
+            $selectedOptions = "None selected";
+        }
+
+        // add data to session array
+        $f3->set('SESSION.name', $name);
+        $f3->set('SESSION.options', $selectedOptions);
+
+        // send the user to next form
+        $f3->reroute('summary');
+    }
 
     // Get the data from the model
     // and add it to the F3 hive
@@ -36,6 +55,16 @@ $f3->route('GET /survey', function($f3) {
     // Render a view page
     $view = new Template();
     echo $view->render('views/survey.html');
+});
+
+// Survey summary
+$f3->route('GET /summary', function($f3) {
+
+    //var_dump($f3->get('SESSION'));
+
+    // render a view page
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 // Run Fat-Free
